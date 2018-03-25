d9041a6d5297b7355d4cb6335cdafefc2a8ad952
mattcharles <matthew.c.anthony.miller@gmail.com>
2018-03-25 18:49:46 -0500

    Tribe support

--- Assets/Scripts/Tribe.cs
+++ Assets/Scripts/Tribe.cs
@@ -1,3 +1,16 @@
+using System;
+
 public enum Tribe : Byte {
-    
+    Dwarf,
+    Undead,
+    Plant,
+    Troll,
+    Ogre,
+    Royal,
+    Beast,
+    Demon,
+    Ship,
+    Pirate,
+    Deepsea,
+    Atlantean
 }
