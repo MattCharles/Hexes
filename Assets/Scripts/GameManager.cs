63ba3c7ec9677beae0a9e3185d3d81d77b1dd7ed
mattcharles <matthew.c.anthony.miller@gmail.com>
2018-03-27 20:36:31 -0500

    Initial UI stuff.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class GameManager : MonoBehaviour
+{
+    private const float TILE_OFFSET = 0.5f;
+
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    public List<GameObject> pawns;
+    private List<GameObject> activePawns;
+
+    private Quaternion orientation = Quaternion.Euler(0, 180, 0);
+
+    public string[] masks = { "PawnLayer", "TileLayer", "HandLayer", "ShopLayer" };
+
+    private void Start()
+    {
+        //TODO: allow other numbers of players
+        Match match = new Match(2);
+        match.Start();
+    }
+
+    private void Update()
+    {
+        UpdateSelection();
+        DrawTiles();
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask(masks)))
+        {
+            Debug.Log(hit.point);
+        }
+    }
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pawn : MonoBehaviour {
+    public Player Owner { get; set; }
+    public Card Card { get; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
